aa43392fde8f48261f41ab763b988dc890b299d5
russellschmidt <mail@russellschmidt.net>
2012-11-26 19:57:06 -0800

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrygSite::Application.config.secret_token = '22c55ef27024bc77be407fad42d61cf2fec143092da8d4a5b7f993f0bb7065a5265841ae5a843a54b81ce0d2d7680cf9cabbd023db83e643aca69de38b32c2d2'
